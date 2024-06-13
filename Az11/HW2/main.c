a4553102f3deac7beef31534c35816a39ce6b9e0
jes1per <157613175+jes1per@users.noreply.github.com>
2024-06-13 12:00:45 +0330

    Update main.c
    Add trademark

--- Az11/HW2/main.c
+++ Az11/HW2/main.c
@@ -1,20 +1,12 @@
 /* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2023 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+/*
+ * ***************************************************************************************
+ * This code is part of projects created by jes1per.
+ * All rights reserved.
+ * Unauthorized copying, modification, or distribution of this code is strictly prohibited.
+ * For more information, visit https://github.com/jes1per
+ * ***************************************************************************************
+ */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
